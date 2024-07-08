e0dd4c6428528a73882a838d13a353813cc48108
Maximino Salgado <130181504+18maxsal@users.noreply.github.com>
2024-07-08 05:48:31 +0000

    added new category for files (i.e. Queue LC Questions)

---
+++ Queue/CircularGameWinner.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        # print("hello")
+        arr = list(range(1, n))
+        # print(arr)
+        return 1
+
+n = 5
+k = 2
+test = Solution()
+result = test.findTheWinner(n, k)
+print(result)
