f847c222f6485dc19299c361831e25dca77a7c37
Maximino Salgado <130181504+18maxsal@users.noreply.github.com>
2024-07-13 07:11:46 +0000

    Deleted unused code in Carfleet.py

--- Stack/Python Version/Carfleet.py
+++ Stack/Python Version/Carfleet.py
@@ -3,7 +3,6 @@
 from typing import List
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        ans = 0
         pair = [[p, s] for p, s in zip(position, speed)]
         stack = []
         for p, s in sorted(pair)[::-1]: # Reverse Sorted Order
