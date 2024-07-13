dbb1caa6aecda4f64f49f0a599af8d974b75cedc
Maximino Salgado <130181504+18maxsal@users.noreply.github.com>
2024-07-13 06:58:13 +0000

    Added lines to Carfleet.py

--- Stack/Python Version/Carfleet.py
+++ Stack/Python Version/Carfleet.py
@@ -3,8 +3,10 @@ from typing import List
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         ans = 0
-        stack = [] # Will be a single array containing both the position and speed of any i'th car
+        stack = [] # Will be a single array containing both the position, speed, and the time it will
+        # take to reach the target for any i'th car
 
         for i in range(len(position)): # Combining position and speed arrays into one single array
-            stack.append(position[i], speed[i])
+            time_till_target = target - position[i]
+            stack.append(position[i], speed[i], time_till_target)
         
