07dfaf683f51d7d9cd0f201f744ca6165b5fb510
Maximino Salgado <130181504+18maxsal@users.noreply.github.com>
2024-07-13 07:10:42 +0000

    Added link of solution video to Carfleet.py

--- Stack/Python Version/Carfleet.py
+++ Stack/Python Version/Carfleet.py
@@ -1,4 +1,5 @@
 # Link to Problem: https://leetcode.com/problems/car-fleet/
+# Link to Solution Video (NeetCode): https://www.youtube.com/watch?v=Pr6T-3yB9RM
 from typing import List
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
