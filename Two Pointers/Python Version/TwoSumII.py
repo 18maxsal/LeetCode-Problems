5b675a5ec38a4fbb1f26f6727d82439c067b8f72
Maximino Salgado <130181504+18maxsal@users.noreply.github.com>
2024-07-17 19:14:21 +0000

    Finished TwoSumII.py

--- Two Pointers/Python Version/TwoSumII.py
+++ Two Pointers/Python Version/TwoSumII.py
@@ -1,11 +1,17 @@
-
+# Link to Problem: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
 from typing import List
-
-
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        for i in range(len(numbers) ):
-            for j in range(i + 1, len(numbers)):
-                if numbers[i] + numbers[j] == target:
-                    return [i + 1, j + 1]
+        start = 0 # Pointer for the start of the array
+        end = len(numbers) - 1 # Pointer for the end of the array 
+        while start <= end: # Iterating through using a two pointer approach
+            if numbers[start] + numbers[end] == target:
+                return [start + 1, end + 1]
+            
+            elif numbers[start] + numbers[end] > target:  # I.E. our resulting number is greater than the target number, we 
+                # can reduce this number by decreasing the index of the end pointer by one
+                end -= 1
+            else: # I.E. our resulting number is smaller than the target number, we can increase this number by 
+                # increasing the index of the start pointer by one 
+                start += 1
