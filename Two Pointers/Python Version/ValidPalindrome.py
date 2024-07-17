17550a68f6a26d216c41e14fbcc6cf5b5cdb1d0a
Maximino Salgado <130181504+18maxsal@users.noreply.github.com>
2024-07-17 06:40:35 +0000

    Finished ValidPalindrome.py with an o(n) solution, can still be optimized

--- Two Pointers/Python Version/ValidPalindrome.py
+++ Two Pointers/Python Version/ValidPalindrome.py
@@ -1,30 +1,30 @@
-
+# Link to Problem: https://leetcode.com/problems/valid-palindrome/
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        
         letters = ""
-        # Getting the letters only from the given string
+
+        # Getting the letters and numbers ONLY from the given string
         for i in s:
             if i.isalpha():
                 letters += i.lower()
+            elif i.isdigit():
+                letters += str(i)
 
         left = 0
         right = len(letters) - 1
-        while left < right:
-            if letters[left] == letters[right]:
+
+        while left < right: # Iterating through both ends of the array until they meet in the middle 
+            if letters[left] != letters[right]:
                 return False
             left += 1
             right -= 1
         
         return True
 
-        # for i in range(len(letters) // 2):
-        #     for j in range (len(letters) // 2, -1):
-        #         if letters[i] != letters[j]:
-        #             return False
-        # return True
-
-s = "A man, a plan, a canal: Panama"
+# s = "A man, a plan, a canal: Panama"
 # s = "race a car"
+s = "0P"
 test = Solution()
 result = test.isPalindrome(s = s)
 print("Result: ", result)
