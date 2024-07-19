9fad913ce33326e58ed5930f4998b687f60579e6
Maximino Salgado <130181504+18maxsal@users.noreply.github.com>
2024-07-19 07:51:10 +0000

    worked on LuckyNumbersInmatrix.py

--- Arrays/Python Versions/LuckyNumbersInMatrix.py
+++ Arrays/Python Versions/LuckyNumbersInMatrix.py
@@ -1,20 +1,31 @@
-
+# Link to Problem: https://leetcode.com/problems/lucky-numbers-in-a-matrix/?envType=daily-question&envId=2024-07-19
 from typing import List
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
 
+        # ans = []
+        # rows_and_columns = [] # Follows the following ordering: [row, column]
+        # for i in range(len(matrix)):
+        #     row = []
+        #     column = []
+        #     for j in range(len(matrix)):
+        #         row.append(matrix[i][j])
+        #         column.append(matrix[j][i])
+        #     rows_and_columns.append([row, column])
+
         ans = []
-        rows_and_columns = [] # Follows the following ordering: [row, column]
+        nums_found = []
         for i in range(len(matrix)):
-            rows_and_columns.append(matrix[i])
-            column = []
+            row_min = []
+            column_max = []
             for j in range(len(matrix)):
-                column.append(matrix[i][j])
-            rows_and_columns.append(column)
+                row_min.append(matrix[i][j])
+                column_max.append(matrix[j][i])
+            nums_found.append([min(row_min), max(column_max)])
         
-        for i in rows_and_columns:
-            pass    
-        
+        for i in nums_found:
+            if i[0] == i[1]:
+                ans.append(i[0])
         return ans
     
     def is_lucky(self, row, column, num):
@@ -23,7 +34,11 @@ class Solution:
         return False
 
 
+matrix = [[3,7,8],[9,11,13],[15,16,17]]
+test = Solution()
+result = test.luckyNumbers(matrix= matrix)
 
+print(result)
 
 
 
