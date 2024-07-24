7f11136a23ee6e961edfce12253fd8e3fb476386
Maximino Salgado <130181504+18maxsal@users.noreply.github.com>
2024-07-24 15:47:58 +0000

    Finished Daily Problem Sort Jumbled Numbers

--- Arrays/Python Versions/SortTheJumbledNumbers.py
+++ Arrays/Python Versions/SortTheJumbledNumbers.py
@@ -3,45 +3,17 @@ from typing import List
 
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        new_nums = {}
+        new_nums = []
 
-        for i in nums: # Converting to the mapped numbers system
-            new_nums[self.jumbledNumber(mapping, str(i))] = i
-
-
-        # Sort the mapped numbers b using quicksort
-        keys_sorted = list(new_nums.keys())
-        low = 0
-        high = len(keys_sorted) - 1
-        self.quickSort(keys_sorted, low, high)
-        print("keys_sorted: ", keys_sorted)
-        # for i in new_nums.keys():
-        #     pass
-
-        return new_nums
-    
-    def partition(self, new_nums, low, high):
-
-        pivot = new_nums[high]
-
-        i = low - 1
-
-        for j in range(low, high):
-            if new_nums[j] <= pivot:
-                i = i + 1
-                new_nums[i], new_nums[j] = new_nums[j], new_nums[i]
-        new_nums[i + 1], new_nums[high] = new_nums[high], new_nums[i + 1]
-
-        return i + 1
-    
-    def quickSort(self, new_nums, low, high):
-        if low < high:
-            pi = self.partition(new_nums, low, high)
-            self.quickSort(new_nums, low, pi - 1)
-            self.quickSort(new_nums, pi + 1, high)
-
+        for i, n in enumerate(nums): # Converting to the mapped numbers system
+            new_nums.append((int(self.jumbledNumber(mapping, str(n))), i))
         
+        new_nums.sort()
+        ans = []
+        for i in new_nums:
+            ans.append(nums[i[1]])
 
+        return ans
 
     def jumbledNumber(self, mapping: List[int], num: str):
         result = ""
