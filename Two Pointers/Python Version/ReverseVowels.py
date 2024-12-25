ddfbe2a60f071c50bda0e6e1cec5115f95a131fe
Maximino Salgado <130181504+18maxsal@users.noreply.github.com>
2024-12-25 21:31:46 +0000

    Added some comments

--- Two Pointers/Python Version/ReverseVowels.py
+++ Two Pointers/Python Version/ReverseVowels.py
@@ -1,24 +1,25 @@
+# Link to problem: https://leetcode.com/problems/reverse-vowels-of-a-string/?envType=study-plan-v2&envId=leetcode-75
 class Solution:
-    def reverseVowels(self, s: str):
+    def reverseVowels(self, s: str): # Using a two pointer approach here
         left = 0
         right = len(s) - 1
         vowels = "aeiouAEIOU"
         s = list(s)
         while left <= right:
-            if s[left] in vowels and s[right] in vowels:
+            if s[left] in vowels and s[right] in vowels: # Case of where the left and right pointer are pointing at vowels
                 s[left], s[right] = s[right], s[left]
                 left += 1
                 right -= 1
                 continue
-            elif s[left] in vowels and s[right] not in vowels:
+            elif s[left] in vowels and s[right] not in vowels: # Case of only left pointer is pointing at a vowel
                 while left <= right and s[right] not in vowels:
                     right -= 1
                 continue
-            elif s[left] not in vowels and s[right] in vowels:
+            elif s[left] not in vowels and s[right] in vowels: # Case of only right pointer is pointing at a vowel
                 while left <= right and s[left] not in vowels:
                     left += 1
                 continue
-            else:
+            else: # Case of NEITHER the left or right pointer is pointing at a vowel
                 left += 1
                 right -= 1
         return "".join(s)
