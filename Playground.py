5c459c2f7c8024e0f0306df6b7790eef3348bb40
Maximino Salgado <maxsalgado2018@gmail.com>
2025-11-06 12:25:07 -0600

    minor changes

--- Playground.py
+++ Playground.py
@@ -1,8 +1,27 @@
-# This is just a file meant to test things out
 
-arr = [[1, 2, 3], 
-       [4, 5, 6],
-       [7, 8, 9]]
+def test(word: str, target1: str, target2: str):
 
-for i in range(len(arr)):
-    print(arr[i])
+    ans = float('inf')
+    target1_index = -1
+    target2_index = -1
+
+    for index, curr_word in enumerate(word):
+
+        if curr_word == target1:
+            target1_index = index
+            if target1_index > -1 and target2_index > -1:
+                ans = min(ans, abs(target1_index - target2_index))
+        
+        elif curr_word == target2:
+            target2_index = index
+            if target1_index > -1 and target2_index > -1:
+                ans = min(ans, abs(target1_index - target2_index))
+    
+    return ans
+
+
+word = ["the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"]
+target1 = "fox"
+target2 = "dog"
+
+print(test(word, target1, target2))
